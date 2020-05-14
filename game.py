eed61214c16231bc0744006ba21734fd2e7dc3eb
Darkellin <42428245+Darkellin@users.noreply.github.com>
2020-05-14 19:04:51 +0600

    Первое

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 # Импортируем модуль под Excel
 import xlrd
+import xlwt
 file = "Test.xlsx"
 # открываем файл Excel
 wb = xlrd.open_workbook(file)
@@ -9,31 +10,19 @@ sheet0 = wb.sheet_by_index(0)
 #Запрашиваем данные, текст вопроса будет у нас в первой колонке. Соответственно переменной у нас будет строка.
 question = 0
 answer = 1
+ListOfAnswers = []
 while question < 5:
         print(sheet0.cell_value(question, 0))
         question += 1
-        answer = 1
+        # модуль, собирающий баллы.
         while answer < 12:
             print(sheet0.cell_value(question, answer))
             answer += 2
-        if
-        
-        
+        a = input("Ваш ответ...")
+        ListOfAnswers.append(a)
+        answer = 1   
+print(ListOfAnswers)
 
-  
-Первый вопрос
-    Первый ответ
-    Второй ответ
-    Третий Ответ
-    Четвертый ответ
-    Пятый ответ
-    Шестой ответ
-Второй вопрос
-    Первый ответ
-    Второй ответ
-    Третий Ответ
-    Четвертый ответ
-    Пятый ответ
-    Шестой ответ
-
-
+my_list = [1, 2]
+my_list.append(13)
+print(my_list)
